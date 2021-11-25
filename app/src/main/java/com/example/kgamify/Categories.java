6b950bd2b8ca0dd36661d95c6abfb871f052e051
rushika-21 <rushika.21810263@viit.ac.in>
2021-11-25 22:47:15 +0530

    Recycler view added

--- app/src/main/java/com/example/kgamify/Categories.java
+++ app/src/main/java/com/example/kgamify/Categories.java
@@ -2,6 +2,7 @@ package com.example.kgamify;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.content.Intent;
@@ -19,10 +20,12 @@ import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 public class Categories extends AppCompatActivity {
 
 
-    ImageView arrow, arrow1;
-    LinearLayout hiddenView, category1, category2, hiddenview1, categoey3;
-    CardView cardView, cardView1;
+    ImageView arrow;
+    LinearLayout hiddenView, category1;
+    CardView cardView;
     Dialog info;
+
+    RecyclerView recyclerView,recyleview_champoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +35,17 @@ public class Categories extends AppCompatActivity {
 
         info = new Dialog(this);    //info popup
 
-        //card1
         cardView = findViewById(R.id.card1);
         arrow = findViewById(R.id.expand_more);
-        hiddenView = findViewById(R.id.hidden_view);
+     //   hiddenView = findViewById(R.id.hidden_view);
         category1 = findViewById(R.id.category1);
-        category2 = findViewById(R.id.category2);
+        recyclerView=(RecyclerView) findViewById(R.id.recyleview);
+        recyleview_champoin=(RecyclerView) findViewById(R.id.recyleview_champoin);
 
 
-        //card2
-        arrow1 = findViewById(R.id.expand_more1);
-        cardView1 = findViewById(R.id.card2);
-        hiddenview1 = findViewById(R.id.hidden_view1);
-        categoey3 = findViewById(R.id.category3);
 
 
-        //card1
+
         arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,7 +74,7 @@ public class Categories extends AppCompatActivity {
             }
         });
 
-        //card1 expand1
+        // expand1 champoinship
         category1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,57 +84,6 @@ public class Categories extends AppCompatActivity {
             }
         });
 
-
-        //card1 expand2
-        category2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in= new Intent(Categories.this,GameMode.class);
-                startActivity(in);
-
-            }
-        });
-
-
-
-        //card2
-        arrow1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // If the CardView is already expanded, set its visibility
-                //  to gone and change the expand less icon to expand more.
-                if (hiddenview1.getVisibility() == View.VISIBLE) {
-
-                    // The transition of the hiddenView is carried out
-                    //  by the TransitionManager class.
-                    // Here we use an object of the AutoTransition
-                    // Class to create a default transition.
-                    TransitionManager.beginDelayedTransition(cardView1, new AutoTransition());
-                    hiddenview1.setVisibility(View.GONE);
-                    arrow1.setImageResource(R.drawable.ic_baseline_expand_more_24);
-                }
-
-                // If the CardView is not expanded, set its visibility
-                // to visible and change the expand more icon to expand less.
-                else {
-
-                    TransitionManager.beginDelayedTransition(cardView1, new AutoTransition());
-                    hiddenview1.setVisibility(View.VISIBLE);
-                    arrow1.setImageResource(R.drawable.ic_baseline_expand_less_24);
-                }
-            }
-        });
-
-        //card2 expand1
-        categoey3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(Categories.this,GameMode.class);
-                startActivity(intent);
-
-            }
-        });
 
 
     }
