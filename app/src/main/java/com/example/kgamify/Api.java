dc753251b68ab7a870b48850a139860a753785e2
Prajwal Suryawanshi <prajwalsuryawanshi12@gmail.com>
2021-12-13 13:17:55 +0530

    drawer coded added

--- app/src/main/java/com/example/kgamify/Api.java
+++ app/src/main/java/com/example/kgamify/Api.java
@@ -1,13 +1,9 @@
 package com.example.kgamify;
 
-import io.reactivex.Observable;
-import io.reactivex.Single;
-import io.reactivex.disposables.Disposable;
-import okhttp3.ResponseBody;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
