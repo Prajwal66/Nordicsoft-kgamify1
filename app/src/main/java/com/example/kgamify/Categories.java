be812981c1f090e113b29678b5f911f0ed34e404
Prajwal Suryawanshi <prajwalsuryawanshi12@gmail.com>
2021-12-21 09:37:48 +0530

    championship search bar removed

--- app/src/main/java/com/example/kgamify/Categories.java
+++ app/src/main/java/com/example/kgamify/Categories.java
@@ -1,4 +1,5 @@
 package com.example.kgamify;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +18,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
-
-
-
 
 
 import com.google.android.material.navigation.NavigationView;
@@ -31,13 +30,14 @@ import io.reactivex.disposables.CompositeDisposable;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
 public class Categories extends AppCompatActivity {
     NavigationView navigationView;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
     DrawerLayout drawers;
     LinearLayout hiddenView, category1;
-    TextView tv_cat_name,tv_champ_name,tv_champ_start_time,textView;
+    TextView tv_cat_name, tv_champ_name, tv_champ_start_time, textView;
     ImageView profile;
     //Api Integration Variables
     CompositeDisposable compositeDisposable = new CompositeDisposable();
@@ -50,15 +50,16 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
         //textView = findViewById(R.id.t1);
         drawers = findViewById(R.id.my_drawer_layout);
         navigationView = findViewById(R.id.nav_view);
+
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawers, R.string.open, R.string.close);
 
         drawers.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-
 
         // to make the Navigation drawer icon always appear on the action bar
         getSupportActionBar().setCustomView(R.layout.custom_action_bar);
@@ -67,16 +68,16 @@ public class Categories extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.menu_categories:
-                        Intent intent = new Intent(Categories.this,Categories.class);
+                        Intent intent = new Intent(Categories.this, Categories.class);
                         startActivity(intent);
                         break;
                     case R.id.menu_Notifications:
-                        Toast.makeText(getApplicationContext(),"NOTIFICATIONS",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "NOTIFICATIONS", Toast.LENGTH_LONG).show();
                         break;
                     case R.id.menu_wallet:
-                        Toast.makeText(getApplicationContext(),"99+",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "99+", Toast.LENGTH_LONG).show();
                         break;
                 }
                 return true;
@@ -93,17 +94,17 @@ public class Categories extends AppCompatActivity {
 
     private void getCategoriesFromApi() {
 
-        api2=RetrofitInstance.getRetrofit().create(Api.class);
-        Call<Backend_CategoryWrapper> call=api2.getBackend_categories();
+        api2 = RetrofitInstance.getRetrofit().create(Api.class);
+        Call<Backend_CategoryWrapper> call = api2.getBackend_categories();
 
         call.enqueue(new Callback<Backend_CategoryWrapper>() {
             @Override
             public void onResponse(Call<Backend_CategoryWrapper> call, Response<Backend_CategoryWrapper> response) {
 
-                category_arr=response.body().getBackend_categories();
+                category_arr = response.body().getBackend_categories();
 
 
-                recycler_view_1.setAdapter(new myAdapter(category_arr,getApplicationContext()));
+                recycler_view_1.setAdapter(new myAdapter(category_arr, getApplicationContext()));
             }
 
             @Override
@@ -126,19 +127,15 @@ public class Categories extends AppCompatActivity {
     private void initialize() {
         //card1
         category1 = findViewById(R.id.fixed_layout_champ);
-        tv_cat_name=(TextView)findViewById(R.id.tv_cat_name);
-        tv_champ_name=(TextView)findViewById(R.id.tv_champ_name);
-        tv_champ_start_time=(TextView)findViewById(R.id.tv_champ_start_time);
+        tv_cat_name = (TextView) findViewById(R.id.tv_cat_name);
+        tv_champ_name = (TextView) findViewById(R.id.tv_champ_name);
+        tv_champ_start_time = (TextView) findViewById(R.id.tv_champ_start_time);
 
-        recycler_view_1=(RecyclerView) findViewById(R.id.recycler_view_1);
-        profile=(ImageView) findViewById(R.id.clickHere);
+        recycler_view_1 = (RecyclerView) findViewById(R.id.recycler_view_1);
+        profile = (ImageView) findViewById(R.id.clickHere);
 
 
     }
-
-
-
-
 
 
 }
--- app/src/main/java/com/example/kgamify/Championships.java
+++ app/src/main/java/com/example/kgamify/Championships.java
@@ -1,6 +1,10 @@
 package com.example.kgamify;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,12 +12,16 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.w3c.dom.Text;
 
@@ -25,7 +33,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Championships extends AppCompatActivity {
-
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayouts;
+    Toolbar toolbar;
     ImageView img_champ_sidemenu;
     SearchView search_view_champ;
     TextView tv_select_champ;
@@ -48,7 +59,41 @@ public class Championships extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_championships);
-        getSupportActionBar().hide();
+
+        nav = findViewById(R.id.nav_view1);
+        drawerLayouts = findViewById(R.id.newdrawer);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayouts, R.string.open, R.string.close);
+        //this is used for side menu bar icon which will open and close the navigation
+
+        drawerLayouts.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_categories:
+                        Intent intent = new Intent(Championships.this, Categories.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.menu_Notifications:
+                        Toast.makeText(getApplicationContext(), "NOTIFICATIONS", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_wallet:
+                        Toast.makeText(getApplicationContext(), "99+", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return true;
+            }
+        });
 
 
 
@@ -63,6 +108,14 @@ public class Championships extends AppCompatActivity {
 
 
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -115,8 +168,6 @@ public class Championships extends AppCompatActivity {
 
     private void initialize() {
 
-        img_champ_sidemenu=(ImageView) findViewById(R.id.img_champ_sidemenu);
-        search_view_champ=(SearchView)  findViewById(R.id.search_view_champ);
         tv_select_champ=(TextView) findViewById(R.id.tv_select_champ);
         recycler_view_2=(RecyclerView) findViewById(R.id.recycler_view_2);
 
